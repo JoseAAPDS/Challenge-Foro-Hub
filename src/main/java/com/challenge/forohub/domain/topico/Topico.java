ac31a31d229576b8f21d3da81903f7a945b720b4
José Acevedo <josearmando.acevedo@gmail.com>
2024-06-11 13:02:58 -0600

    feat: added Topico entity

--- src/main/java/com/challenge/forohub/domain/topico/Topico.java
+++ src/main/java/com/challenge/forohub/domain/topico/Topico.java
@@ -1,20 +1,34 @@
 package com.challenge.forohub.domain.topico;
 
 import com.challenge.forohub.domain.curso.Curso;
+import com.challenge.forohub.domain.respuesta.Respuesta;
 import com.challenge.forohub.domain.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode (of = "id")
+@Table (name = "topicos")
+@Entity (name = "topico")
 public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String mensaje;
     private LocalDateTime fechaCreacion;
     private Boolean activo;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "usuario_id")
     private Usuario autor;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "curso_id")
     private Curso curso;
     private List<Respuesta> respuestas;
-
-
-
 }
