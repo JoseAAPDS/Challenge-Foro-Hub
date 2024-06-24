606eb0d6aea332a36cebf914a8d56b9c38938b1e
José Acevedo <josearmando.acevedo@gmail.com>
2024-06-24 16:06:53 -0600

    fix:remove unique from fiel contrasena in Usuario

--- src/main/resources/db/migration/V1__create_table_usuarios.sql
+++ src/main/resources/db/migration/V1__create_table_usuarios.sql
@@ -2,7 +2,7 @@ CREATE TABLE usuarios (
     id BIGINT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     correo_electronico VARCHAR(100) NOT NULL UNIQUE,
-    contrasena VARCHAR(100) NOT NULL UNIQUE,
+    contrasena VARCHAR(100) NOT NULL,
     perfil VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
 );
